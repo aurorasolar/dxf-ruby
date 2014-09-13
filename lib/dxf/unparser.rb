f4a747ff3b0fc9b2662610f9b86900123d7d17ba
Meseker Yohannes <meseker.yohannes@gmail.com>
2014-09-12 17:43:40 -0700

    added code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polylineadded code 5 and 330 for polyline

--- lib/dxf/unparser.rb
+++ lib/dxf/unparser.rb
@@ -48,11 +48,13 @@ module DXF
             # }
             code = [
                 0, 'LWPOLYLINE',
+				5, 1E4,
+				330, 1F,
 				100, 'AcDbEntity',
 				8, layer,
                 100, 'AcDbPolyline',
 				90, points.length,
-                70, 0,
+                70, 1,
 				43, 0
             ]
 
