9a194a3cb2186e838e134137ba0b063a3e84de2b
Meseker Yohannes <meseker.yohannes@gmail.com>
2014-09-15 19:34:37 -0700

    changed way points are passed from rectangle to pline

--- lib/dxf/unparser.rb
+++ lib/dxf/unparser.rb
@@ -199,12 +199,11 @@ module DXF
                     # element.edges.map {|edge| line(edge.first, edge.last, layer, transformation) + set_options(element.options) }
                     pline(element.vertices, layer, transformation) + set_options(element.options)
                 when Geometry::Rectangle
-					debugger
                     point0, point2 = *element.points
                     point1 = Geometry::Point[point2.x, point0.y]
                     point3 = Geometry::Point[point0.x, point2.y]
                     # element.edges.map {|edge| line(edge.first, edge.last, layer, transformation) + set_options(element.options) }
-                    pline([point0, point1, point2, point3], layer, transformation) + set_options(element.options)
+                    pline(element.points, layer, transformation) + set_options(element.options)
                 when Geometry::Square
                     points = element.points
                     points.each_cons(2).map {|p1,p2| line(p1,p2, layer, transformation) + set_options(element.options) } + line(points.last, points.first, layer, transformation) + set_options(element.options)
