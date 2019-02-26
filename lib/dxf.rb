704f5cdc284df34303e74e735a9ea27ee0a1737f
Arindam Bose <arindam.1993@gmail.com>
2019-02-25 18:46:19 -0800

    fix/precision-syntax: add comment as per CR suggestion

--- lib/dxf.rb
+++ lib/dxf.rb
@@ -12,6 +12,7 @@ Reading and writing of files using AutoCAD's {http://en.wikipedia.org/wiki/AutoC
     # @param [String] filename	The path to write to
     # @param [Sketch] sketch	The {Sketch} to export
     # @param [Symbol] units	Convert all values to the specified units (:inches or :mm)
+    # @param [Number] precison	number of decimal places to keep for the practional component
     def self.write(sketch, units=:mm, layers, precision)
 	# File.open(filename, 'w') {|f| Unparser.new(units).unparse(f, sketch, layers)}
     Unparser.new(units, precision).unparse(sketch, layers)
